53f0f183d882ee5079d7409ade40f5e8ff976493
Devin Ivy <devin@bigroomstudios.com>
2022-05-09 00:33:42 -0400

    Support node v18, drop node v12

---
+++ test/esm.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const Code = require('@hapi/code');
+const Lab = require('@hapi/lab');
+
+
+const { before, describe, it } = exports.lab = Lab.script();
+const expect = Code.expect;
+
+
+describe('import()', () => {
+
+    let Statehood;
+
+    before(async () => {
+
+        Statehood = await import('../lib/index.js');
+    });
+
+    it('exposes all methods and classes as named imports', () => {
+
+        expect(Object.keys(Statehood)).to.equal([
+            'Definitions',
+            'default',
+            'exclude',
+            'prepareValue'
+        ]);
+    });
+});
